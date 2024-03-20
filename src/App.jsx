b48c2fd5574036fe7b6dbba8bf53aae0b3a93aae
hasan-tec <131611130+hasan-tec@users.noreply.github.com>
2024-03-20 22:19:48 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import CalculationOverview from "./components/CalculationOverview";
+import Input from "./components/Input";
+
+export default function App() {
+  return (
+    <>
+      <h1>Multiplied by two App</h1>
+
+      <Input />
+
+      <CalculationOverview />
+    </>
+  );
+}
